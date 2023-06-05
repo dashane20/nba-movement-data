a13cfb64ca549e3ce269400a451e81cc8438697b
dashane20 <dashane20@gmail.com>
2023-06-05 11:03:23 +0200

    filepath changes
    changing relative to my os

--- movement/constant.py
+++ movement/constant.py
@@ -1,6 +1,6 @@
-import os
+#import os
 # change this data_dir for personal path
-if os.environ['HOME'] == '/home/neil':
-    data_dir = '/home/neil/projects/nba-movement-data'
-else:
-    raise Exception("Unspecified data_dir, unknown environment")
+#if os.environ['HOME'] == '/home/neil':
+data_dir = r'C:\Users\dasha\Documents\GitHub\TheTwoManGame\nba-movement-data'
+#else:
+#    raise Exception("Unspecified data_dir, unknown environment")
